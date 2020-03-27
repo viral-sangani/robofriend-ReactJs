30467d7d84febebd27c42d2adf1284ff8cd1236c
viral-sangani <viral.sangani2011@gmail.com>
2020-03-27 21:15:35 +0530

    Updates

---
+++ precache-manifest.341e563370f35c5b17fde253fbfa794d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c2c220cd7c8dff82d5e6e262e1d622",
+    "url": "/robofriend-ReactJs/index.html"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/robofriend-ReactJs/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "bcfb375068dd22e96fc8",
+    "url": "/robofriend-ReactJs/static/css/main.1f125317.chunk.css"
+  },
+  {
+    "revision": "79436e81eb103ede0f87",
+    "url": "/robofriend-ReactJs/static/js/2.8ca0bda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend-ReactJs/static/js/2.8ca0bda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfb375068dd22e96fc8",
+    "url": "/robofriend-ReactJs/static/js/main.11b744b3.chunk.js"
+  },
+  {
+    "revision": "b27932e6e02d919b844d",
+    "url": "/robofriend-ReactJs/static/js/runtime-main.823750b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a57c96cc69b5d652c60d83c019ebc682.js"
+  "/robofriend-ReactJs/precache-manifest.341e563370f35c5b17fde253fbfa794d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriend-ReactJs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
