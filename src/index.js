cf6d1e05b341980a813f294168fcec025843b4aa
viral-sangani <viral.sangani2011@gmail.com>
2020-03-28 00:52:51 +0530

    redux-logger added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import "tachyons";
 import App from "./containers/App";
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
